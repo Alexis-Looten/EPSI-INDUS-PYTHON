dea742ff30081dd9b7280223e0cad837688ee8e8
alexis.looten <alexis.looten@pingflow.fr>
2022-11-28 14:05:23 +0100

    Inital commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Projet : EPSI-INDUS-PYTHON
+
+## GROUPE : LOOTEN Alexis - AMELETE Zita
+
+----------------------------------------------------------
+
+### *How to use the Python script :*
+
+Install Selenium (pip install selenium)
+
+Open main.py --> edit url1, url2 and url3 with your urls
+
+Run the script, it will show you the 3 url's title and finaly the longest of them
+
+----------------------------------------------------------
+
+### *Selenium project*
+
+This repo contain the selenium project .side with the 3 IDE test/scenario :
+
+- test_documentation
+- test_inscription
+- test_titre_contenu
---
+++ main.py
@@ -0,0 +1,40 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+url1 = "https://www.humblebundle.com/"
+url2 = "https://www.nautiljon.com/"
+url3 = "https://www.youtube.com/"
+
+def get_title(driver, url, i):
+    driver.set_window_size(1600, 1200)
+    driver.get(url)
+    
+    driver.implicitly_wait(2)
+    title = driver.title
+    print(f"Titre récupéré n°{i}: {title}")
+    
+    return title
+
+def create_chrome_driver():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('headless')
+    return webdriver.Chrome(
+        options=chrome_options
+    )
+
+
+if __name__ == "__main__":
+    driver = create_chrome_driver()
+    
+    url_list = [url1, url2, url3]
+    title_list = []
+    
+    for url in url_list:
+        title = get_title(driver, url, url_list.index(url)+1)
+        title_list.append(title)
+    
+    driver.quit()
+    
+    print(f"Voici le titre le plus long : '{max(title_list, key=len)}'")
